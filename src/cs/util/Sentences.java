d9f2227b367ab29246f250f0f0f1b75635469759
kingdom-of-fife <fr27@st-andrews.ac.uk>
2018-04-19 15:36:42 +0100

    Add a new sentence

--- src/cs/util/Sentences.java
+++ src/cs/util/Sentences.java
@@ -8,7 +8,8 @@ public class Sentences {
         builder.append("Offer a low-cost renin-angiotensin-aldosterone system antagonist to people with CKD and hypertension and an ACR of 30 mg/mmol or more (ACR category A3).\n");
         builder.append("Offer a low-cost renin-angiotensin-aldosterone system antagonist to people with CKD and an ACR of 70 mg/mmol or more (irrespective of hypertension or cardiovascular disease).\n");
         builder.append("Control blood pressure to targets of 120-139/<90 mmHg in people without diabetes with ACR < 70 mg/mmol.\n");
-        builder.append("Control blood pressure to targets of 120-129/<80 mmHg in people with diabetes or with ACR >= 70 mg/mmol.");
+        builder.append("Control blood pressure to targets of 120-129/<80 mmHg in people with diabetes or with ACR >= 70 mg/mmol.\n");
+        builder.append("Offer colecalciferol or ergocalciferol to people who also have vitamin D deficiency if vitamin D supplementation is indicated in people with CKD.");
 
         return builder.toString();
     }
